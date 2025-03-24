fe2e7ef48cebf0d5432d0ace5a128514ab81cb61
cranoo <75974473+cranoo3@users.noreply.github.com>
2025-03-24 21:23:18 +0900

    Initial Commit

---
+++ PlayOGGSample/PlayOGGSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayOGGSampleApp.swift
+//  PlayOGGSample
+//
+//  Created by cranoo on 2025/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayOGGSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlayOGGSampleTests/PlayOGGSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayOGGSampleTests.swift
+//  PlayOGGSampleTests
+//
+//  Created by cranoo on 2025/03/24.
+//
+
+import Testing
+@testable import PlayOGGSample
+
+struct PlayOGGSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
